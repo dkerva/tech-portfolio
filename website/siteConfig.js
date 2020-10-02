ebc75f9527c4580e2551402e8b06cebccccace20
dkerva <dkalkunte@gmail.com>
2020-10-02 15:43:52 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Deepa Kalkunte' // Title for your website.
-    tagline: 'getting comfortable being uncomfortable',
+    tagline: 'getting comfortable being uncomfortable', // notsure why this is not working
     profilePhoto: 'img/profile.png',
     url: 'https://dkerva.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
